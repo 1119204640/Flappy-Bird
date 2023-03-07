0e121a3c4367cc08b8dce847a491ed3eaa1f04f9
LEON <61219917+1119204640@users.noreply.github.com>
2023-03-08 01:24:20 +0800

    no message

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,7 @@ public class Player : MonoBehaviour
     private Vector3 direction;
     public float gravity = -9.8f;
     public float strength = 5f;
+    public float tilt = 5f;
 
     private SpriteRenderer spriteRenderer;
     public Sprite[] sprites;
@@ -48,6 +49,10 @@ public class Player : MonoBehaviour
 
         direction.y += gravity * Time.deltaTime;
         transform.position += direction * Time.deltaTime;
+
+        Vector3 rotation = transform.eulerAngles;
+        rotation.z = direction.y * tilt;
+        transform.eulerAngles = rotation;
     }
 
     private void AnimateSprites()
